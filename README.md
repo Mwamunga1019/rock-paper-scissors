76f0a72ab0e08334cd6213109922e5abce396036
Mwamunga <evansbrigids254@gmail.com>
2022-07-30 20:43:20 +0300

    README.md, style.css, index.html, script.js and fonts/

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Make your function’s playerSelection parameter case-insensitive (so users can 
 Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 
 Use prompt() to get input from the user.
+
+# part-2
+
+In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+
+For now, remove the logic that plays exactly five rounds.
+
+Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+
+Add a div for displaying results and change all of your console.logs into DOM methods.
+
+Display the running score, and announce a winner of the game once one player reaches 5 points.
--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let playerScore = 0;
 let compScore = 0;
 
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+const outcomeDiv = document.querySelector('.outcome');
+const compS = document.querySelector('.comp-score');
+const playerS = document.querySelector('.player-score');
+
+
+
+
 function computerPlay() {
     const pick = ['rock', 'paper', 'scissors'];
     return pick[Math.floor(Math.random() * pick.length)];
@@ -13,46 +23,104 @@ function computerPlay() {
 
 
 function playRound(playerSelection, computerSelection) {
+    const p = document.createElement('p');
     if (playerSelection === computerSelection) {
-        return alert((`You tied! You both picked ${playerSelection}`));
+        p.innerText = `You tied! You both picked ${playerSelection}`;
+        
     } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore++;
-        return alert('You win! Rock crushes scissors');
+        p.innerText = 'You win! Rock crushes scissors';
+       
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore++;
-        return alert('You win! Paper covers rock');
+        p.innerText = 'You win! Paper covers rock';
+        
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         playerScore++;
-        return alert('You win! Scissors cut paper');
-    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+        p.innerText = 'You win! Scissors cuts paper';
+        
+     } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
         compScore++;
-        return alert('You lose! Rock crushes scissors');
+        p.innerText = 'You lose! Rock crushes scissors';
+       
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         compScore++
-        return alert('You lose! Paper covers rock');
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        p.innerText = 'You lose! Paper covers rock';
+        
+     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         compScore++;
-        return alert('You lose! Scissors cut paper');
+        p.innerText = 'You lose! Scissors cuts paper';
+        
     } 
-    
+
+    outcomeDiv.appendChild(p);
 
     
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Type either rock, paper or scissors. The game plays for 5 rounds', 'rock').toLowerCase();
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-
-    if (playerScore > compScore) {
-        return 'You beat the Computer. You are a genius!';
-    } else if (playerScore < compScore) {
-        return 'You got beat by the computer. Work on your throws!';
-    } else {
-        return 'You tied with the computer. Quite Shabby!';
-    }
+function runningScore(playerS, compS) {
+    playerS.innerText = `Player Score: ${playerScore}`;
+    compS.innerText = `Computer Score: ${compScore}`;
 }
 
-alert(game());
+function checkForWinner(playerScore, compScore) {
+    const h2 = document.createElement('h2');
+    if (playerScore === 3) {
+        
+        h2.classList.add('player-won');
+        h2.innerText = `You won ${playerScore} to ${compScore}. Great job beating the computer ` ;
+        
+    }
+
+    if (compScore === 3) {
+        h2.classList.add('computer-won');
+        h2.innerText = `You loose ${playerScore} to ${compScore}. Up your throw game! `;
+        
+    }
+    
+    outcomeDiv.append(h2);
+}
+
+rockButton.addEventListener('click', () => {
+    const computerSelection = computerPlay();
+    const playerSelection = 'rock';
+    playRound(playerSelection, computerSelection);
+    runningScore(playerS, compS);
+    checkForWinner(playerScore, compScore);
+})
+
+paperButton.addEventListener('click', () => {
+    const computerSelection = computerPlay();
+    const playerSelection = 'paper';
+    playRound(playerSelection, computerSelection);
+    runningScore(playerS, compS);
+    checkForWinner(playerScore, compScore);
+})
+
+scissorsButton.addEventListener('click', () => {
+    const computerSelection = computerPlay();
+    const playerSelection = 'scissors';
+    playRound(playerSelection, computerSelection);
+    runningScore(playerS, compS);
+    checkForWinner(playerScore, compScore);
+    
+})
+
+
+// function game() {
+//     // for (let i = 0; i < 5; i++) {
+//     //     const playerSelection = prompt('Type either rock, paper or scissors. The game plays for 5 rounds', 'rock').toLowerCase();
+//     //     const computerSelection = computerPlay();
+//     //     console.log(playRound(playerSelection, computerSelection));
+//     // }
+
+//     if (playerScore > compScore) {
+//         return 'You beat the Computer. You are a genius!';
+//     } else if (playerScore < compScore) {
+//         return 'You got beat by the computer. Work on your throws!';
+//     } else {
+//         return 'You tied with the computer. Quite Shabby!';
+//     }
+// }
+
+// alert(game());
